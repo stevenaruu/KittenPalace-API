39c9ff600f6ca4184026a262a34784edb690e324
Steven Forsythia <stvnhd164@gmail.com>
2023-09-02 20:29:28 +0700

    chore: add controller for getHealth

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -1,9 +1,6 @@
-import { Router, Request, Response, NextFunction } from 'express'
-import { logger } from '../utils/logger'
+import { Router } from 'express'
+import { getHealth } from '../controllers/health.controller'
 
 export const HealthRouter: Router = Router()
 
-HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-    logger.info('Health check success')
-    res.status(200).send({ status: '200', message: 'OK' })
-})
+HealthRouter.get('/', getHealth)
